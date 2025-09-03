7ed163856c9688d9073b2d9eb4970533e1566369
haguettaz <hugaguett@gmail.com>
2025-09-03 18:07:08 +0200

    max violations and signal energy

--- rsnn_plugin/__init__.py
+++ rsnn_plugin/__init__.py
@@ -15,15 +15,15 @@ LIB = Path(__file__).parent
 
 
 def scan_coef_0(
-    prev_delta: IntoExprColumn,
+    prev_length: IntoExprColumn,
     prev_coef_1: IntoExprColumn,
-    weight_0: IntoExprColumn,
+    in_coef_0: IntoExprColumn,
 ) -> pl.Expr:
     return register_plugin_function(
         args=[
-            prev_delta,
+            prev_length,
             prev_coef_1,
-            weight_0,
+            in_coef_0,
         ],
         plugin_path=LIB,
         function_name="scan_coef_0",
@@ -31,27 +31,85 @@ def scan_coef_0(
     )
 
 
-def scan_coef_1(prev_delta: IntoExprColumn, weight_1: IntoExprColumn) -> pl.Expr:
+def scan_coef_1(prev_length: IntoExprColumn, in_coef_1: IntoExprColumn) -> pl.Expr:
     return register_plugin_function(
-        args=[prev_delta, weight_1],
+        args=[prev_length, in_coef_1],
         plugin_path=LIB,
         function_name="scan_coef_1",
         is_elementwise=False,
     )
 
 
+def energy_syn_to_syn_metric(f_time, start_syn_i, start_syn_j) -> pl.Expr:
+    return register_plugin_function(
+        args=[
+            f_time,
+            start_syn_i,
+            start_syn_j,
+        ],
+        plugin_path=LIB,
+        function_name="energy_syn_to_syn_metric",
+        is_elementwise=True,
+    )
+
+
+def energy_rec_to_syn_metric(f_time, start_rec, start_syn) -> pl.Expr:
+    return register_plugin_function(
+        args=[
+            f_time,
+            start_rec,
+            start_syn,
+        ],
+        plugin_path=LIB,
+        function_name="energy_rec_to_syn_metric",
+        is_elementwise=True,
+    )
+
+
+def max_violation(
+    start: IntoExprColumn,
+    length: IntoExprColumn,
+    coef_0: IntoExprColumn,
+    coef_1: IntoExprColumn,
+    *,
+    vmax: float,
+) -> pl.Expr:
+    return register_plugin_function(
+        args=[start, length, coef_0, coef_1],
+        plugin_path=LIB,
+        function_name="max_violation",
+        kwargs={"vmax": vmax},
+        is_elementwise=False,
+        returns_scalar=True,
+    )
+
+
 def first_ftime(
     start: IntoExprColumn,
-    delta: IntoExprColumn,
-    prev_delta: IntoExprColumn,
-    weight_0: IntoExprColumn,
-    weight_1: IntoExprColumn,
+    length: IntoExprColumn,
+    prev_length: IntoExprColumn,
+    in_coef_0: IntoExprColumn,
+    in_coef_1: IntoExprColumn,
     f_thresh: IntoExprColumn,
 ) -> pl.Expr:
     return register_plugin_function(
-        args=[start, delta, prev_delta, weight_0, weight_1, f_thresh],
+        args=[start, length, prev_length, in_coef_0, in_coef_1, f_thresh],
         plugin_path=LIB,
         function_name="first_ftime",
         is_elementwise=False,
         returns_scalar=True,
+    )
+
+
+def extend_periodically(
+    time: IntoExprColumn,
+    period: IntoExprColumn,
+    tmin: IntoExprColumn,
+    tmax: IntoExprColumn,
+) -> pl.Expr:
+    return register_plugin_function(
+        args=[time, period, tmin, tmax],
+        plugin_path=LIB,
+        function_name="extend_periodically",
+        is_elementwise=True,
     )
--- src/expressions.rs
+++ src/expressions.rs
@@ -1,67 +1,73 @@
 #![allow(clippy::unused_unit)]
 
+use core::f64;
+
 use itertools::izip;
 use lambert_w::{lambert_w0, lambert_wm1};
 use polars::prelude::*;
 use pyo3_polars::derive::polars_expr;
 use pyo3_polars::export::polars_core::utils::CustomIterTools;
+use serde::Deserialize;
 
-fn same_output_type(input_fields: &[Field]) -> PolarsResult<Field> {
-    let field = &input_fields[0];
-    Ok(field.clone())
-}
+// fn same_output_type(input_fields: &[Field]) -> PolarsResult<Field> {
+//     let field = &input_fields[0];
+//     Ok(field.clone())
+// }
 
-#[polars_expr(output_type_func=same_output_type)]
+#[polars_expr(output_type=Float64)]
 fn scan_coef_0(inputs: &[Series]) -> PolarsResult<Series> {
-    let prev_delta = inputs[0].f64()?;
+    let prev_length = inputs[0].f64()?;
     let prev_coef_1 = inputs[1].f64()?;
-    let weight_0 = inputs[2].f64()?;
+    let in_coef_0 = inputs[2].f64()?;
 
-    let out: Float64Chunked = izip!(
-        prev_delta.iter(),
-        prev_coef_1.iter(),
-        weight_0.iter()
-    )
-    .scan(
-        0_f64,
-        |state: &mut f64,
-         (prev_delta, prev_coef_1, weight_0): (Option<f64>, Option<f64>, Option<f64>)| {
-            match prev_delta {
-                Some(prev_delta) => {
-                    *state = (*state + prev_coef_1.unwrap_or(0.0) * prev_delta)
-                        * (-prev_delta).exp()
-                        + weight_0.unwrap_or(0.0);
-                    Some(Some(*state))
+    let out: Float64Chunked =
+        izip!(prev_length.iter(), prev_coef_1.iter(), in_coef_0.iter())
+            .scan(
+                0_f64,
+                |coef_0: &mut f64,
+                 (prev_length, prev_coef_1, in_coef_0): (
+                    Option<f64>,
+                    Option<f64>,
+                    Option<f64>,
+                )| {
+                    match prev_length {
+                        Some(prev_length) => {
+                            *coef_0 = (*coef_0 + prev_coef_1.unwrap_or(0.0) * prev_length)
+                                * (-prev_length).exp()
+                                + in_coef_0.unwrap_or(0.0);
+                            Some(Some(*coef_0))
+                        },
+                        None => {
+                            *coef_0 = in_coef_0.unwrap_or(0.0);
+                            Some(Some(*coef_0))
+                        },
+                    }
                 },
-                None => {
-                    *state = weight_0.unwrap_or(0.0);
-                    Some(Some(*state))
-                },
-            }
-        },
-    )
-    .collect_trusted();
+            )
+            .collect_trusted();
 
     Ok(out.into_series())
 }
 
-#[polars_expr(output_type_func=same_output_type)]
+#[polars_expr(output_type=Float64)]
 fn scan_coef_1(inputs: &[Series]) -> PolarsResult<Series> {
-    let prev_delta = inputs[0].f64()?;
-    let weight_1 = inputs[1].f64()?;
+    let prev_length = inputs[0].f64()?;
+    let in_coef_1 = inputs[1].f64()?;
 
-    let out: Float64Chunked = izip!(prev_delta.iter(), weight_1.iter())
+    let out: Float64Chunked = izip!(prev_length.iter(), in_coef_1.iter())
         .scan(
             0_f64,
-            |state: &mut f64, (prev_delta, weight_1): (Option<f64>, Option<f64>)| match prev_delta {
-                Some(prev_delta) => {
-                    *state = *state * (-prev_delta).exp() + weight_1.unwrap_or(0.0);
-                    Some(Some(*state))
-                },
-                None => {
-                    *state = weight_1.unwrap_or(0.0);
-                    Some(Some(*state))
-                },
+            |coef_1: &mut f64, (prev_length, in_coef_1): (Option<f64>, Option<f64>)| {
+                match prev_length {
+                    Some(prev_length) => {
+                        *coef_1 = *coef_1 * (-prev_length).exp() + in_coef_1.unwrap_or(0.0);
+                        Some(Some(*coef_1))
+                    },
+                    None => {
+                        *coef_1 = in_coef_1.unwrap_or(0.0);
+                        Some(Some(*coef_1))
+                    },
+                }
             },
         )
         .collect_trusted();
@@ -69,12 +75,155 @@ fn scan_coef_1(inputs: &[Series]) -> PolarsResult<Series> {
 }
 
 #[polars_expr(output_type=Float64)]
+fn energy_syn_to_syn_metric(inputs: &[Series]) -> PolarsResult<Series> {
+    let f_time = inputs[0].f64()?;
+    let start_syn_i = inputs[1].f64()?;
+    let start_syn_j = inputs[2].f64()?;
+
+    let out: Float64Chunked = izip!(f_time.iter(), start_syn_i.iter(), start_syn_j.iter())
+        .map(
+            |(f_time, start_syn_i, start_syn_j)| match (f_time, start_syn_i, start_syn_j) {
+                (Some(f_time), Some(start_syn_i), Some(start_syn_j)) => {
+                    let length = (f_time - start_syn_i).min(f_time - start_syn_j);
+                    let isi = (start_syn_j - start_syn_i).abs();
+                    let fading = (-2.0 * length).exp();
+                    let int_1 = 1.0 - (1.0 + 2.0 * length) * fading;
+                    let int_2 = 1.0 - (1.0 + 2.0 * length * (length + 1.0)) * fading;
+                    Some((int_2 + isi * int_1) * (-isi).exp() / 4.0)
+                },
+                _ => None,
+            },
+        )
+        .collect_trusted();
+
+    Ok(out.into_series())
+}
+
+#[polars_expr(output_type=Float64)]
+fn energy_rec_to_syn_metric(inputs: &[Series]) -> PolarsResult<Series> {
+    let f_time = inputs[0].f64()?;
+    let start_rec = inputs[1].f64()?;
+    let start_syn = inputs[2].f64()?;
+
+    let out: Float64Chunked = izip!(f_time.iter(), start_rec.iter(), start_syn.iter())
+        .map(
+            |(f_time, start_rec, start_syn)| match (f_time, start_rec, start_syn) {
+                (Some(f_time), Some(start_rec), Some(start_syn)) => {
+                    if start_syn > start_rec {
+                        let length = f_time - start_syn;
+                        let isi = start_syn - start_rec;
+                        let int_1 = 1.0 - (1.0 + 2.0 * length) * (-2.0 * length).exp();
+                        Some(int_1 * (-isi).exp() / 4.0)
+                    } else {
+                        Some(0.0)
+                    }
+                },
+                _ => None,
+            },
+        )
+        .collect_trusted();
+
+    Ok(out.into_series())
+}
+
+fn f64_list_dtype(input_fields: &[Field]) -> PolarsResult<Field> {
+    let field = Field::new(
+        input_fields[0].name().clone(),
+        DataType::List(Box::new(DataType::Float64)),
+    );
+    Ok(field.clone())
+}
+
+#[polars_expr(output_type_func=f64_list_dtype)]
+fn extend_periodically(inputs: &[Series]) -> PolarsResult<Series> {
+    let time = inputs[0].f64()?;
+    let period = inputs[1].f64()?;
+    let tmin = inputs[2].f64()?;
+    let tmax = inputs[3].f64()?;
+
+    let out: ListChunked = izip!(time.iter(), period.iter(), tmin.iter(), tmax.iter())
+        .map(|(time, period, tmin, tmax)| {
+            let mut res = Vec::new();
+            if let (Some(time), Some(period), Some(tmin), Some(tmax)) = (time, period, tmin, tmax) {
+                let kmin = -((time - tmin) / period).floor() as i64;
+                let kmax = -((time - tmax) / period).ceil() as i64;
+                for k in kmin..=kmax {
+                    res.push(time + k as f64 * period);
+                }
+            }
+            Series::new(PlSmallStr::EMPTY, res)
+        })
+        .collect_trusted();
+
+    Ok(out.into_series())
+}
+
+#[derive(Deserialize)]
+struct MaxViolationKwargs {
+    vmax: f64,
+}
+
+#[polars_expr(output_type=Float64)]
+fn max_violation(inputs: &[Series], kwargs: MaxViolationKwargs) -> PolarsResult<Series> {
+    let start = inputs[0].f64()?;
+    let length = inputs[1].f64()?;
+    let coef_0 = inputs[2].f64()?;
+    let coef_1 = inputs[3].f64()?;
+
+    let res = izip!(start.iter(), length.iter(), coef_0.iter(), coef_1.iter())
+        .filter_map(|(start, length, coef_0, coef_1)| match start {
+            Some(start) => {
+                let coef_0 = coef_0.unwrap_or(0.0);
+                let coef_1 = coef_1.unwrap_or(0.0);
+
+                match length {
+                    Some(length) => {
+                        let dt = 1_f64 - coef_0 / coef_1;
+                        if (coef_1 > 0.0) & (dt >= 0.0) & (dt < length) {
+                            let v = (coef_0 + coef_1 * dt) * (-dt).exp();
+                            Some((start + dt, v))
+                        } else {
+                            let vstart = coef_0;
+                            let vend = (coef_0 + coef_1 * length) * (-length).exp();
+                            if vstart > vend {
+                                Some((start, vstart))
+                            } else {
+                                Some(((start + length - 1e-12).max(start), vend))
+                            }
+                        }
+                    },
+                    None => {
+                        let dt = 1_f64 - coef_0 / coef_1;
+                        if (coef_1 > 0.0) & (dt >= 0.0) {
+                            Some((start + dt, (coef_0 + coef_1 * dt) * (-dt).exp()))
+                        } else {
+                            if coef_0 >= 0.0 {
+                                Some((start, coef_0))
+                            } else {
+                                Some((f64::INFINITY, 0.0))
+                            }
+                        }
+                    },
+                }
+            },
+            None => None,
+        })
+        .max_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+
+    let tmax: Option<f64> = match res {
+        Some((tmax, vmax)) if vmax > kwargs.vmax => Some(tmax),
+        _ => None,
+    };
+    Ok(Series::new(PlSmallStr::EMPTY, vec![tmax]))
+}
+
+#[polars_expr(output_type=Float64)]
 fn first_ftime(inputs: &[Series]) -> PolarsResult<Series> {
     let start = inputs[0].f64()?;
-    let delta = inputs[1].f64()?;
-    let prev_delta = inputs[2].f64()?;
-    let weight_0 = inputs[3].f64()?;
-    let weight_1 = inputs[4].f64()?;
+    let length = inputs[1].f64()?;
+    let prev_length = inputs[2].f64()?;
+    let in_coef_0 = inputs[3].f64()?;
+    let in_coef_1 = inputs[4].f64()?;
     let f_thresh = inputs[5].f64()?;
 
     let mut coef_0 = 0_f64;
@@ -82,28 +231,25 @@ fn first_ftime(inputs: &[Series]) -> PolarsResult<Series> {
 
     let ftime = izip!(
         start.iter(),
-        delta.iter(),
-        prev_delta.iter(),
-        weight_0.iter(),
-        weight_1.iter(),
+        length.iter(),
+        prev_length.iter(),
+        in_coef_0.iter(),
+        in_coef_1.iter(),
         f_thresh.iter(),
     )
     .find_map(
-        |(start, delta, prev_delta, weight_0, weight_1, threshold)| match (start, threshold) {
+        |(start, length, prev_length, in_coef_0, in_coef_1, threshold)| match (start, threshold) {
             (Some(start), Some(threshold)) => {
-                (coef_0, coef_1) = match prev_delta {
-                    Some(prev_delta) => {
-                        coef_0 = weight_0.unwrap_or(0.0)
-                            + (coef_0 + coef_1 * prev_delta) * (-prev_delta).exp();
-                        coef_1 = weight_1.unwrap_or(0.0) + coef_1 * (-prev_delta).exp();
-                        (coef_0, coef_1)
-                    },
-                    None => (weight_0.unwrap_or(0.0), weight_1.unwrap_or(0.0)),
-                };
+                let in_coef_0 = in_coef_0.unwrap_or(0.0);
+                let in_coef_1 = in_coef_1.unwrap_or(0.0);
 
-                // println!(
-                //     "start: {start} prev_delta: {prev_delta:?} delta: {delta:?} coef_0: {coef_0}, coef_1: {coef_1}"
-                // );
+                (coef_0, coef_1) = match prev_length {
+                    Some(prev_length) => (
+                        in_coef_0 + (coef_0 + coef_1 * prev_length) * (-prev_length).exp(),
+                        in_coef_1 + coef_1 * (-prev_length).exp(),
+                    ),
+                    None => (in_coef_0, in_coef_1),
+                };
 
                 let f_time = {
                     if coef_0 < threshold {
@@ -123,9 +269,9 @@ fn first_ftime(inputs: &[Series]) -> PolarsResult<Series> {
                     }
                 };
 
-                match delta {
-                    Some(delta) => {
-                        if (f_time >= start) && (f_time < start + delta) {
+                match length {
+                    Some(length) => {
+                        if (f_time >= start) && (f_time < start + length) {
                             Some(f_time)
                         } else {
                             None
@@ -145,3 +291,72 @@ fn first_ftime(inputs: &[Series]) -> PolarsResult<Series> {
     );
     Ok(Series::new(PlSmallStr::EMPTY, vec![ftime]))
 }
+
+// fn struct_coef_dtype(input_fields: &[Field]) -> PolarsResult<Field> {
+//     let field = &input_fields[0];
+//     match field.dtype() {
+//         DataType::Struct(fields) => Ok(Field::new(
+//             "struct_coef".into(),
+//             DataType::Struct(fields.clone()),
+//         )),
+//         dtype => polars_bail!(InvalidOperation: "Expected Struct type, got {dtype:?}"),
+//     }
+// }
+
+// #[derive(Debug, Clone, Copy)]
+// struct Coef {
+//     c0: f64,
+//     c1: f64,
+// }
+
+// #[polars_expr(output_type_func=struct_coef_dtype)]
+// fn scan_coef(inputs: &[Series]) -> PolarsResult<Series> {
+//     let prev_delta = inputs[0].f64()?;
+//     let weight = inputs[1].struct_()?;
+
+//     let coef_results: Vec<Coef> = prev_delta
+//         .iter()
+//         .zip(weight.iter())
+//         .scan(
+//             Coef { c0: 0.0, c1: 0.0 },
+//             |state: &mut Coef, (prev_delta, weight): (Option<f64>, Option<Coef>)| match (
+//                 prev_delta, weight,
+//             ) {
+//                 (Some(prev_delta), Some(weight)) => {
+//                     state.c0 = weight.c0 + (state.c0 + state.c1 * prev_delta) * (-prev_delta).exp();
+//                     state.c1 = weight.c1 + state.c1 * (-prev_delta).exp();
+//                     Some(*state)
+//                 },
+//                 (None, Some(weight)) => {
+//                     state.c0 = weight.c0;
+//                     state.c1 = weight.c1;
+//                     Some(*state)
+//                 },
+//                 (Some(prev_delta), None) => {
+//                     state.c0 = (state.c0 + state.c1 * prev_delta) * (-prev_delta).exp();
+//                     state.c1 = state.c1 * (-prev_delta).exp();
+//                     Some(*state)
+//                 },
+//                 (None, None) => {
+//                     state.c0 = 0.0;
+//                     state.c1 = 0.0;
+//                     Some(*state)
+//                 },
+//             },
+//         )
+//         .collect();
+
+//     let c0_values: Vec<f64> = coef_results.iter().map(|c| c.c0).collect();
+//     let c1_values: Vec<f64> = coef_results.iter().map(|c| c.c1).collect();
+
+//     let c0_series = Series::new("c0".into(), c0_values);
+//     let c1_series = Series::new("c1".into(), c1_values);
+
+//     let out = StructChunked::from_series("struct_coef".into(), [c0_series, c1_series].iter())?;
+//     Ok(out.into_series())
+// }
+
+// fn same_dtype(input_fields: &[Field]) -> PolarsResult<Field> {
+//     let field = &input_fields[0];
+//     Ok(field.clone())
+// }
